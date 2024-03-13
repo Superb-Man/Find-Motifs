92d92fe91373497ff0db6b44b64750403bce8eac
Superb-Man <kazitoriqe207@gmail.com>
2024-03-14 04:45:04 +0600

    using only entropy

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from algo import *
+from algo import algo
 
 def main():
     dna = []
     # read the input file
-    with open('hm03.txt', 'r') as f:
+    with open('data/hm03.txt', 'r') as f:
         dna = f.read().splitlines()
     
     for i in range(8,25):
         print(i)
-        motif = multipleSeedSearch(dna, 100, i, 1000,gibbsSampler)
-        print(motif, Entropy(motif),Score(motif))
+        motif = algo.multipleSeedSearch(dna, 100, i, 1000,algo.gibbsSampler)
+        print(motif, algo.Entropy(motif),algo.Score(motif))
         print("\n")
